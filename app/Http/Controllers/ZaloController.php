e07ebe93e7ba4c0073540f4bd8536cc8c2117f74
nhtung <dev.nhtung@gmail.com>
2025-09-11 21:09:33 +0700

    fix ip viet nam12

--- app/Http/Controllers/ZaloController.php
+++ app/Http/Controllers/ZaloController.php
@@ -25,7 +25,7 @@ class ZaloController extends Controller
             'access_token' => $accessToken,
             'code' => $code,
             'secret_key' => $secretKey,
-        ])->get('https://nckh.hueic.edu.vn/zalo-api/v2.0/me/info');
+        ])->get('https://nckh.hueic.edu.vn/zalo-api/me/info');
 
         // Xử lý response
         if ($response->successful()) {
